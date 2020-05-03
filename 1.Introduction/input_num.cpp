1b2b1c13a636400625ae591b32eca06bdddacc13
Dhruv Arora <dhruv.arora@nutanix.com>
2020-05-03 16:18:53 +0530

    Add introduction Of C++

---
+++ 1.Introduction/input_num.cpp
@@ -0,0 +1,19 @@
+/* Author : Dhruv Arora
+ This program will take input a number from user and will print the number
+ on console
+ */
+
+#include <iostream>
+using namespace std
+
+int main()
+{
+  // Declare the variable
+  int num;
+  cout<<"Enter the num :";
+  // Take input from user.
+  cin>>num;
+  // Display the output on console
+  cout<<"\n The number entered by user is :"<<num;
+  return 0;
+}
